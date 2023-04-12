d7fb23cc3f62b152d8a2e8141de9a431b5a07509
jacobsss <kuba.spala@op.pl>
2023-04-12 15:46:06 +0200

    Tiding up the headers

--- P1_MES/mylib_precond.cpp
+++ P1_MES/mylib_precond.cpp
@@ -1,6 +1,5 @@
 #include "mylib_precond.h"
 
-/*Calculates a residuum of a set of equations*/
 void res(int N, double** A, double* x, double* b, double* r) {
 	for (int i = 0; i < N; i++) {
 		r[i] = b[i];
@@ -10,7 +9,7 @@ void res(int N, double** A, double* x, double* b, double* r) {
 		}
 	}
 }
-/*Calculates and returns the form od a vector*/
+
 double norm(int N, double* res) {
 	double norm = 0;
 	for (int i = 0; i < N; i++) norm = norm + res[i]*res[i];
@@ -19,14 +18,11 @@ double norm(int N, double* res) {
 	return norm;
 }
 
-/*Sets simplified vector p the same as residuum*/
 void precond_i(int N, double** A, double* r, double* p) {
 	for (int i = 0; i < N; i++) p[i] = r[i];
 }
 
-/*Simplified solve for testing*/
-solve_XXX(int N, double* A, double* x, double* b, int iter_max, double eps);
+void solve_simpl(int N, double* A, double* x, double* b, int iter_max, double eps);
 
-/*General solve with a pointer to function*/
-solve(int N, double* A, double* x, double* b, int iter_max, double eps,
+void solve(int N, double* A, double* x, double* b, int iter_max, double eps,
 	void (*precond)(int N, double** A, double* r, double* p));
--- P1_MES/mylib_precond.h
+++ P1_MES/mylib_precond.h
@@ -5,12 +5,20 @@
 #include <stdlib.h>
 #include <math.h>
 
+/*Calculates a residuum of a set of equations*/
 void res(int N, double** A, double* x, double* b, double* r);
 
+/*Calculates and returns the form od a vector*/
 double norm(int N, double* res);
 
+/*Sets simplified vector p the same as residuum*/
 void precond_i(int N, double** A, double* r, double* p);
 
-void solve(int N);
+/*Simplified solve for testing*/
+void solve_simpl(int N, double* A, double* x, double* b, int iter_max, double eps);
+
+/*General solve with a pointer to function*/
+void solve(int N, double* A, double* x, double* b, int iter_max, double eps,
+	void (*precond)(int N, double** A, double* r, double* p));
 
 #endif
