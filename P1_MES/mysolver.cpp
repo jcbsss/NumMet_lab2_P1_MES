2d9c696ccdf223317d5ff5aefafaef32fa6f6fba
jacobsss <kuba.spala@op.pl>
2023-04-13 23:03:13 +0200

    Added othe preconditioners

--- P1_MES/mysolver.cpp
+++ P1_MES/mysolver.cpp
@@ -22,6 +22,15 @@ void precond_i(int N, double** A, double* r, double* p) {
 	for (int i = 0; i < N; i++) p[i] = r[i];
 }
 
+void precond_Jacobi(int N, double** A, double* r, double* p) {
+	for (int i = 0; i < N; i++) p[i] = r[i]/A[i][i];
+}
+
+void precond_GS(int N, double** A, double* r, double* p) {
+	p[0] = r[0] / A[0][0];
+	for (int i = 1; i < N; i++) p[i] = 1/A[i][i]*(r[i]+00000000000.1);//ten precon dokoñczyæ
+}
+
 void solve_simpl(int N, double* A, double* x, double* b, int iter_max, double eps);
 
 void solve(int N, double* A, double* x, double* b, int iter_max, double eps,
